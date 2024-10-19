9ea6e387fc82d84658f416cffe7b10170dfa2561
PovilasDuo <duobapovilas@gmail.com>
2024-10-19 14:59:21 +0300

    Added pagination

--- sliumslium.client/src/pages/HomePage/HomePage.tsx
+++ sliumslium.client/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,10 @@
 import { JobOffer } from "../../api/apiModel";
 import useFetch from "../../api/useDataFetching";
 import Filters from "./Filters";
-import JobOfferCard from "../../components/JobOfferCard";
-import { useNavigate } from "react-router";
 import { useState } from "react";
+import Pagination from "./Pagination";
 
 export default function HomePage() {
-  const navigate = useNavigate();
   const { data } = useFetch<JobOffer[]>("JobOffer");
   const [jobOffers, setJobOffers] = useState<JobOffer[]>([]);
 
@@ -21,32 +19,9 @@ export default function HomePage() {
         />
         <div className="job-offers-list" style={{ marginTop: "2rem" }}>
           {jobOffers.length > 0 ? (
-            jobOffers.map((offer) => (
-              <JobOfferCard
-                key={offer.id_JobOffer}
-                offer={offer}
-                handleCardClick={() => {
-                  navigate(`/job-offer/${offer.id_JobOffer}`, {
-                    state: { offer },
-                  });
-                }}
-              />
-            ))
+            <Pagination jobOffers={jobOffers} />
           ) : (
-            <>
-              {data &&
-                data.map((offer, index) => (
-                  <JobOfferCard
-                    key={index}
-                    offer={offer}
-                    handleCardClick={() => {
-                      navigate(`/job-offer/${offer.id_JobOffer}`, {
-                        state: { offer },
-                      });
-                    }}
-                  />
-                ))}
-            </>
+            data && <Pagination jobOffers={data} />
           )}
         </div>
       </div>
